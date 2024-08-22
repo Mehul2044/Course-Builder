76dd4155a40453358c3f194ceaa364a39e25e006
Mehul Agarwal <mehu2003@gmail.com>
2024-08-22 21:50:44 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import {Button} from "@/components/ui/button.jsx";
+
+function App() {
+
+  return <>
+      <p className={"text-4xl"}>Hello World!</p>
+      <Button>Click me</Button>
+  </>
+}
+
+export default App;
